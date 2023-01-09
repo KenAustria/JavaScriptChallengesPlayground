4d597157ba8e431ff1ae5019ba148438531a209b
Ken Austria <kenaustriadev@gmail.com>
2023-01-09 20:07:52 +0900

    curry time=O(n) space=O(n)

--- curry.js
+++ curry.js
@@ -1,12 +1,16 @@
-// implement a curry() function, which accepts a function and return a curried one.
+/*
+Complexity: Time = O(n) , Space = O(n)
+https://javascript.info/currying-partials
+implement a curry() function, which accepts a function and return a curried one.
+*/
 
 const curry = fn => {
 	// curried collects arguments as an array
 	return function curried(...args) {
 		// if curried args passed equals the number of args that fn expects
 		// then spread args elements to pass into fn
-		if (args.length >= fn.length) return fn(...args)
+		if (args.length >= fn.length) return fn(...args);
 		// otherwise, return curried to collect the next arguments passed
-		return (...args2) => curried(...args, args2)
-	}
-}
+		return (...args2) => curried(...args, args2);
+	};
+};
